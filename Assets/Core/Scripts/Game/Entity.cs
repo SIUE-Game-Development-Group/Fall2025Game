f7421b90cd338db9c5a6b316f497ff578d7d4cfd
Sean <seansdilday@gmail.com>
2025-09-18 18:45:07 -0500

    Weapons' hitboxes can only hit entities once per attack

--- Assets/Core/Scripts/Game/Entity.cs
+++ Assets/Core/Scripts/Game/Entity.cs
@@ -10,7 +10,19 @@ namespace Core.Scripts.Game
     {
         [SerializeField] float _maxHealth = 10f;
         [SerializeField] float _moveSpeed = 5f;
-    
+
+        [Tooltip("These hitboxes may be temporarily disabled when taking damage (temporary invincibility)")]
+        [SerializeField] private Hurtbox[] _hurtboxes;
+
+        [Tooltip("Invincibility time when taking damage")]
+        [SerializeField] private float hitInvincibilityTime = 1.5f;
+
+        /// <summary>
+        /// This value is true while the player is invincible due to temporary hit invincibility
+        /// </summary>
+        /// <returns></returns>
+        private bool _hitInvincible;
+
         Rigidbody2D _rb;
 
         /// <summary>
@@ -50,10 +62,36 @@ namespace Core.Scripts.Game
             _health -= amount;
             Debug.Log(gameObject.name + " damaged " + amount);
             OnDamaged?.Invoke();
-            
+
             if (_health <= 0) {
                 _health = 0;
+                Death();
                 OnDeath?.Invoke();
+            } else if (!_hitInvincible && hitInvincibilityTime > 0f) {
+                StartCoroutine(TemporaryInvincibility());
+            }
+        }
+
+        public virtual void Death() {
+
+        }
+
+        public IEnumerator TemporaryInvincibility() {
+            // Make all hurtboxes invincible
+            _hitInvincible = true;
+            foreach (var hurtbox in _hurtboxes) {
+                hurtbox.StartInvincibility();
+            }
+
+            // Wait until not invincible anymore
+            yield return new WaitForSeconds(hitInvincibilityTime);
+
+            // Make all hurtboxes not invincible anymore.
+            _hitInvincible = false;
+            foreach (var hurtbox in _hurtboxes) {
+                hurtbox.EndInvincibility();
+                // If invincibility down causes taking damage which causes invincibility up, stop the loop
+                if (_hitInvincible) break;
             }
         }
     }
--- Assets/Core/Scripts/Game/Hitbox.cs
+++ Assets/Core/Scripts/Game/Hitbox.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace Core.Scripts.Game
 {
@@ -6,15 +7,21 @@ namespace Core.Scripts.Game
     /// Deals damage to hurtboxes that collide with this.
     /// Hitbox colliders should be triggers.
     /// </summary>
-    [RequireComponent(typeof(Collider2D))]
     public class Hitbox : MonoBehaviour
     {
         public float damage;
+
+        public HashSet<Entity> entitiesHit = new();
+
         
         public void OnTriggerEnter2D(Collider2D other) 
         {
             if (other.gameObject.TryGetComponent(out Hurtbox hurtbox))
             {
+                // If this hitbox has already hit the target entity during this attack, don't hit them again until the next attack is used and the entity is cleared from this set
+                if (entitiesHit.Contains(hurtbox.Entity)) return;
+                entitiesHit.Add(hurtbox.Entity);
+
                 // Will deal damage immediately if the hurtbox is not currently invincible.
                 // If it is currently invincible, will only deal damage when it loses invincibility and still contacting
                 hurtbox.HitboxEntered(this);
@@ -27,6 +34,13 @@ namespace Core.Scripts.Game
             {
                 hurtbox.HitboxExited(this);
             }
+        }
+
+        /// <summary>
+        /// Call when the attack starts so that it can hit any enemy only once
+        /// </summary>
+        public void ResetHit() {
+            entitiesHit.Clear();
         }
     }
 }
