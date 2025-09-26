d491fec39955ae1f44498f62a100c6ece0ee8c9f
Jack Caesar <jackachulian@gmail.com>
2025-09-25 19:03:07 -0500

    Fixed enemy hitbox only hurting player once

--- Assets/Core/Scripts/Game/Hitbox.cs
+++ Assets/Core/Scripts/Game/Hitbox.cs
@@ -13,13 +13,18 @@ namespace Core.Scripts.Game
 
         public HashSet<Entity> entitiesHit = new();
 
+        /// <summary>
+        /// If true, this hitbox can only hit a hurtbox once and then it disables itself until re-enabled. 
+        /// Etc. hitboxes on a sword weapon should hit only once
+        /// </summary>
+        public bool hitOnce = false;
         
         public void OnTriggerEnter2D(Collider2D other) 
         {
             if (other.gameObject.TryGetComponent(out Hurtbox hurtbox))
             {
                 // If this hitbox has already hit the target entity during this attack, don't hit them again until the next attack is used and the entity is cleared from this set
-                if (entitiesHit.Contains(hurtbox.Entity)) return;
+                if (hitOnce && entitiesHit.Contains(hurtbox.Entity)) return;
                 entitiesHit.Add(hurtbox.Entity);
 
                 // Will deal damage immediately if the hurtbox is not currently invincible.
