f3bc5e5a4c34325b001da2c80fe87c71c1ae847c
SoveriegnHippo <zacharymueller3208@gmail.com>
2025-10-23 18:29:04 -0500

    Updated player Animation script

--- Assets/Features/Enemies/TestScripts/zachary/president.cs
+++ Assets/Features/Enemies/TestScripts/zachary/president.cs
@@ -1,13 +1,18 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class president : MonoBehaviour
 {
     public GameObject player;
     public GameObject bulletPrefab;
+
+    [SerializeField] private List<Sprite> sprites;
+    private SpriteRenderer spriteRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -17,6 +22,8 @@ public class president : MonoBehaviour
         {
             Attack();
         }
+        spriteRenderer.sprite = sprites[0];
+
     }
 
 
