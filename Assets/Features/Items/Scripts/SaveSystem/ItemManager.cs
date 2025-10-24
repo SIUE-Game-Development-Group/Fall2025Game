267ef4f1d1f0a340b4a47c3f4fdd9f60e5b93c48
pulsar0569 <jjro0687@gmail.com>
2025-10-24 00:07:10 -0500

    Add functions by name + rework save/load

--- Assets/Features/Items/Scripts/SaveSystem/ItemManager.cs
+++ Assets/Features/Items/Scripts/SaveSystem/ItemManager.cs
@@ -4,22 +4,25 @@ using System.Collections.Generic;
 using Core.Scripts.Game;
 using Features.MainCharacter.Scripts;
 using Object = UnityEngine.Object;
+using UnityEngine.InputSystem.Processors;
+using Unity.VisualScripting;
 
 public class ItemManager : MonoBehaviour
 {
-    
+
     /* TODO:
             1. Make function to swap passives and update the Inventory List accordingly
             2. Rework invenotry system to support multiple weapons and passives rather than set slots
                 - Create different lists for weapons/coins, and collected passives/active items
      */
-    
-    
+
+
     // What the player currently holds
 
-    public static List<Item> inventory;
+    public List<Item> inventory;
+    public List<Item> passives;
 
-    [SerializeField] static List<Item> AllItems = new List<Item>();
+    public static List<Item> AllItems = new List<Item>();
 
     private string targetDirectoryPath = "Assets/Resources/Weapons";
 
@@ -28,14 +31,15 @@ public class ItemManager : MonoBehaviour
 
     public void Start()
     {
-        // Set size of array to 3;
-        inventory = new List<Item>(new Item[3]); // 0 = item, 1 = passive, 2 = currency
+        // Set size of array to 2
+        inventory = new List<Item>(new Item[2]); // 0 = item, 1 = currency
+
+        passives = new List<Item>();
 
         // Load all items into memory
         LoadItems();
 
         // If damage totem equipped, buff damage
-
         GameObject damageTotemInstance = GameObject.Find("DamageTotem");
 
         if (damageTotemInstance != null)
@@ -48,28 +52,26 @@ public class ItemManager : MonoBehaviour
 
         // Start with random item
         SwapItem(RandomGenerateItem().name);
-
-        // Load weapon after swap
-        PlayerAttack playerAttack = GameObject.FindWithTag("Player").GetComponent<PlayerAttack>();
-        inventory[0] = playerAttack.equippedWeapon;
         
-        // Load passive slot with nothing
-        inventory[1] = new Item("PassiveItemID", "Temp Passive Item", "A placeholder for a passive item", Item.Rarity.Common, 1);
-
-        // Load currency slot with 0
-        inventory[2] = new Item("GoldItemID", "Gold", "Player's Currency", Item.Rarity.Common, 0);
+        // Give player passives on start (REMOVE WHEN DONE TESTING)
+        AddPassive("BloodHoundTotem");
+        AddPassive("CrystalLeech");
         
+        // Test save items on start (REMOVE WHEN DONE TESTING)
+        SaveLoadManager manager = new SaveLoadManager();
+        manager.Save();
+
     }
-    
+
     // Scan all files in weapon directory and set id = (weapons path)
     public void LoadItems()
-    {        
+    {
         var allAssets = Resources.LoadAll("", typeof(Item));
 
         foreach (Object asset in allAssets)
         {
             Debug.Log($"Asset Name: {asset.name}, Type: {asset.GetType().Name}");
-            
+
             Item item = asset as Item;
 
             if (item == null) return;
@@ -79,8 +81,31 @@ public class ItemManager : MonoBehaviour
             AllItems.Add(item);
         }
     }
+    
+    // Add passive by string name of actual item (NOT by item name but gameobject name!!!!)
+    public void AddPassive(string itemName)
+    {
+        // Find item by name and make sure it exists
+        Item passiveItem = FindItemByName(itemName);
+        if (passiveItem == null)
+        {
+            Debug.LogError("ItemManager - AddPassive: Item not found!");
+            return;
+        }
 
-    public void SwapItem(string itemName, string beginningFolderName="Weapons/")
+        // Create item in scene
+        GameObject instantiatedPassive = Instantiate(passiveItem.gameObject);
+        // Remove "clone" suffix
+        instantiatedPassive.name = instantiatedPassive.name.Replace("(Clone)", "");
+
+        // Apply newly created item to player
+        instantiatedPassive.transform.parent = GameObject.FindWithTag("Player").transform;
+
+        // Update list of passives
+        passives.Add(passiveItem);
+    }
+
+    public void SwapItem(string itemName)
     {
         PlayerAttack playerAttackScript = this.gameObject.GetComponent<PlayerAttack>();
 
@@ -93,7 +118,7 @@ public class ItemManager : MonoBehaviour
         }
 
         // Load item prefab into unity scene
-        swapItemPrefab = Resources.Load<GameObject>(beginningFolderName + itemSwap.name);
+        swapItemPrefab = Resources.Load<GameObject>("Weapons/" + itemSwap.name);
         if (swapItemPrefab != null)
         {
             Debug.Log("Successfully loaded swap item");
@@ -110,13 +135,13 @@ public class ItemManager : MonoBehaviour
         }
 
         // ~ ~ ~ ~ ~ ~ Apply buffs from totems if they exist ~ ~ ~ ~ ~ ~ ~
-       
+
         // Apply weapon rate buff if it exists
         GameObject weaponRateTotemInstance = GameObject.Find("AttackRateTotem");
         if (weaponRateTotemInstance != null)
         {
             // Run totem rate decrease function with newly equipped weapon
-            weaponRateTotemInstance.GetComponent<Attack_Rate>().DecreaseRateOfWeapon(playerAttackScript.equippedWeapon);;
+            weaponRateTotemInstance.GetComponent<Attack_Rate>().DecreaseRateOfWeapon(playerAttackScript.equippedWeapon); ;
         }
 
         // Apply weapon damage buff if it exists
@@ -136,7 +161,7 @@ public class ItemManager : MonoBehaviour
     }
 
     // Returns item if found, else returns null
-    public Item FindItemByName(string itemName)
+    public static Item FindItemByName(string itemName)
     {
         foreach (Item item in AllItems)
         {
--- Assets/Features/Items/Scripts/SaveSystem/SaveLoadManager.cs
+++ Assets/Features/Items/Scripts/SaveSystem/SaveLoadManager.cs
@@ -3,6 +3,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 using System.Collections.Generic;
 using Core.Scripts.Game;
+using Unity.VisualScripting;
 
 public class SaveLoadManager : MonoBehaviour
 {
@@ -12,46 +13,101 @@ public class SaveLoadManager : MonoBehaviour
             SwapItem function in ItemManager script!`
     */
 
-    string path;
-    
+    private string defaultPath;
+    public static string weaponSaveName;
+    public static string passiveSaveName;
+
+    public void Start()
+    {
+        defaultPath = Application.persistentDataPath;
+        passiveSaveName = "/passiveSave.dat";
+        weaponSaveName = "/weaponSave.dat";
+    }
+
     public void Save()
     {
-        BinaryFormatter formatter = new BinaryFormatter();
-        
-        if (path == null) path = Application.persistentDataPath + "/inventorySave.dat";
-        
-        FileStream stream = new FileStream(path, FileMode.Create);
+        // Fallback if not found or tried to call function from other script before start was called
+        if (defaultPath == null) defaultPath = Application.persistentDataPath;
+        if (passiveSaveName == null) passiveSaveName = "/passiveSave.dat";
+        if (weaponSaveName == null) weaponSaveName = "/weaponSave.dat";
 
-        if (ItemManager.inventory == null)
+        ItemManager itemManager = GameObject.FindWithTag("Player").GetComponent<ItemManager>();
+
+        Debug.Log($"Save path: {defaultPath}/...");
+
+        // Path locations
+        string weaponPath = defaultPath + weaponSaveName;
+        string passiveSavePath = defaultPath + passiveSaveName;
+
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        // Creating save files at given paths
+        FileStream weaponStream = new FileStream(weaponPath, FileMode.Create);
+        FileStream passiveStream = new FileStream(passiveSavePath, FileMode.Create);
+
+        // Ensure the data we're saving is not null
+        if (itemManager.inventory == null)
         {
             Debug.LogError("SaveLoadManager: Could not save because ItemManager.inventory is null!");
             return;
         }
-        
-        formatter.Serialize(stream, ItemManager.inventory);
-        stream.Close();
+        if (itemManager.passives == null)
+        {
+            Debug.LogError("SaveLoadManager: Could not save because ItemManager.passives is null!");
+            return;
+        }
+
+        // Write out encrypted files
+        formatter.Serialize(weaponStream, itemManager.inventory);
+        formatter.Serialize(passiveStream, itemManager.passives);
+
+        // Close streams after we're done writing
+        weaponStream.Close();
+        passiveStream.Close();
+
         Debug.Log("Game Saved!");
     }
 
     public void Load()
     {
-        if (File.Exists(path))
+
+        string inventoryPath = defaultPath + weaponSaveName;
+        string passivePath = defaultPath + passiveSaveName; 
+
+        ItemManager itemManager = GameObject.FindWithTag("Player").GetComponent<ItemManager>();
+
+        // Load weapons inventory
+        if (File.Exists(inventoryPath))
         {
+            // Open file and overwrite inventory list
             BinaryFormatter formatter = new BinaryFormatter();
-            FileStream stream = new FileStream(path, FileMode.Open);
+            FileStream stream = new FileStream(inventoryPath, FileMode.Open);
+            itemManager.inventory = formatter.Deserialize(stream) as List<Item>;
 
-            ItemManager.inventory = formatter.Deserialize(stream) as List<Item>;
+            // Swap weapons after updating inventory list
+            itemManager.SwapItem(itemManager.inventory[0].name);
 
-            // Swap current weapons here!!
-            ItemManager manager = new ItemManager();
-            manager.SwapItem(ItemManager.inventory[0].name);
-            
             stream.Close();
             Debug.Log("Game Loaded!");
         }
         else
         {
-            Debug.LogWarning("Save file not found.");
+            Debug.LogWarning("Inventory Save file not found.");
+        }
+        
+        // Load passives
+        if (File.Exists(passivePath))
+        {
+            // Open file and overwrite passives list
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(passivePath, FileMode.Open);
+            itemManager.passives = formatter.Deserialize(stream) as List<Item>;
+
+            // Load saved passives onto player
+            foreach (Item passive in itemManager.passives)
+            {
+                itemManager.AddPassive(passive.gameObject.name);
+            }
         }
     }
 }
