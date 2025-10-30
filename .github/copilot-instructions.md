6fbac96c6f91a60793ab18a818947b7460a54196
kidleo725 <kindenmcawsome@gmail.com>
2025-10-30 17:43:24 -0500

    Self destruct added

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -2,7 +2,7 @@
 - Project name: Fall2025Game
 - Unity version: Unity 6000.0.57f1
 - Active game object:
-  - Name: Isometric Diamond
+  - Name: Bullet
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Features/Enemies/TestScripts/Kinden/Schreecher.cs
+++ Assets/Features/Enemies/TestScripts/Kinden/Schreecher.cs
@@ -1,26 +1,29 @@
 using UnityEngine;
+using Core.Scripts.Game;
 
-public class Schreecher : MonoBehaviour
+public class Schreecher : Enemy
 {
     public float speed;
     public float bulletSpeed = 50f;
     public GameObject player;
     public GameObject bulletPrefab;
+    private Rigidbody2D rb;
+    public float stopDistance;
+    public GameObject Explosion;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public virtual void Start()
     {
-        
+        base.Start();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    public virtual void Update()
     {
+        base.Update();
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Attack();
-
-
-
         }
         movement();
     }
@@ -39,10 +42,21 @@ public class Schreecher : MonoBehaviour
     void movement()
     { 
         Vector3 playerPosition = player.transform.position;
-        Vector3 direction = playerPosition - transform.position;
-        transform.position += direction.normalized * Time.deltaTime * speed;
-        
+        Vector2 direction = playerPosition - transform.position;
+        float distance = direction.magnitude;
+        if (distance <= stopDistance)
+        {
+            Move(Vector2.zero);
+            GameObject explosion;
+            explosion = Instantiate(Explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Move(direction);
+        }
 
+           // rb.linearVelocity = direction.normalized * speed;
 
 
 
