0d28e36b3f5e1322c2f661c305edbeee9088dcad
MrUnknown1v <maxpereze1@gmail.com>
2025-11-13 15:54:22 -0600

    Edited BulletScript  to have random time  modifier

--- Assets/Features/Items/Scripts/Secondary/BulletScript.cs
+++ Assets/Features/Items/Scripts/Secondary/BulletScript.cs
@@ -6,12 +6,23 @@ public class BulletScript : MonoBehaviour
 {
 
     [SerializeField] private GameObject hitbox;
-      
+    [SerializeField] private bool isRandom = false;  
     float i = 0;
     [SerializeField] private float BulletTime = 5;
+    [SerializeField] private float minTime = 2;
+    [SerializeField] private float maxTime = 3;
+    
 
 void Start()
 {
+        if (isRandom == true)
+        {
+            BulletTime = Random.Range(minTime, maxTime);
+        }
+        else
+        {
+            
+        }
     
 }
 
